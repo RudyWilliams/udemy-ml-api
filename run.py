618ce6a85e921603ef535620d1cbf57177b0dfee
Rudy Williams <rudysw05@knights.ucf.edu>
2020-06-23 15:34:20 -0400

    section 7.3 updates: config/log/tests

---
+++ run.py
@@ -0,0 +1,7 @@
+from api.app import create_app
+from api.config import DevelopmentConfig
+
+application = create_app(config_obj=DevelopmentConfig)
+
+if __name__ == "__main__":
+    application.run()
