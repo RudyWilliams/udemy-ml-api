7b80d18ace1a59c2618f5f074f9333389d0365b3
Rudy Williams <rudysw05@knights.ucf.edu>
2020-06-23 17:47:11 -0400

    section 7.5 updates: version endpoint and test

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -5,10 +5,23 @@ from regression_pipeline.config import config as model_config
 from regression_pipeline.processing.data_management import load_dataset
 from regression_pipeline import __version__ as _version
 
+from api import __version__ as api_version
+
 
 def test_health_endpoint_returns_200(flask_test_client):
     response = flask_test_client.get("/health")
     assert response.status_code == 200
+
+
+def test_version_endpoint_returns_version(flask_test_client):
+    # when
+    response = flask_test_client.get("/version")
+
+    # then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    assert response_json["model_version"] == _version
+    assert response_json["api_version"] == api_version
 
 
 def test_prediction_endpoint_returns_prediction(flask_test_client):
